821179a720c2b941bb886c3aebb5fb6451139083
thematthewmullins <thematthewmullins@gmail.com>
2018-12-06 11:47:00 -0500

    Mod 2 final

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const userData = fs.readFileSync(
 const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Account Summary'}, accounts)
+    res.render('index', { title: 'Account Summary', accounts });
 });
 
 app.get('/savings', (req, res) => {
